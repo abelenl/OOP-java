e2eaa91d2374a1aeea2b9526d41b874dbf7579a5
abelenl <anabelen.llorente@dxc.com>
2024-01-05 13:06:58 +0100

    añadir clases mensajeria

---
+++ BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/MensajeriaJDBCRepo.java
@@ -0,0 +1,27 @@
+package com.banana.bananawhatsapp.persistencia;
+
+import com.banana.bananawhatsapp.modelos.Mensaje;
+import com.banana.bananawhatsapp.modelos.Usuario;
+
+import java.sql.SQLException;
+import java.util.List;
+import lombok.*;
+
+@Setter
+@Getter
+public class MensajeriaJDBCRepo implements IMensajeRepository{
+    @Override
+    public Mensaje crear(Mensaje mensaje) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Mensaje> obtener(Usuario usuario) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean borrarTodos(Usuario usuario) throws SQLException {
+        return false;
+    }
+}
